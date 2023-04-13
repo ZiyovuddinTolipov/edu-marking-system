6bf679ccf385469378ebc2f7dc4f7f019ed5c04c
tolipov <ziyovuddin.developer@gmail.com>
2023-04-14 04:00:51 +0500

    first commit

--- js/app.js
+++ js/app.js
@@ -1,24 +1,10 @@
-let techerReating = document.getElementById("techerReating");
-teacher.onsubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData(teacher);
+let response = await fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits');
 
-    console.log("Form Data");
-    for (let obj of formData) {
-        console.log(obj[0], obj[1]);
-        techerReating.innerHTML == `
-        <tr>
-        <td>${obj[1]}</td>
-        <th>
-          <p>
-            ${obj[1]}
-          </p>
-        </th>
-        <th className="ht">
+// получить один заголовок
+alert(response.headers.get('Content-Type')); // application/json; charset=utf-8
 
-        </th>
-      </tr>
-      `
-
-    }
-};
+// перебрать все заголовки
+for (let [key, value] of response.headers) {
+  alert(`${key} = ${value}`);
+}
+alert("sss")
